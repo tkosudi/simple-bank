899203134ff9dec0c59c7a94ff4f29537a4fefc9
TK <tkosudi@gmail.com>
2024-07-17 18:35:09 -0300

    test(accounts): add test for retrieving an account by ID

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"simplebank/util"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -31,4 +32,16 @@ func createRandomAccount(t *testing.T) Account {
 
 func TestCreateAccount(t *testing.T) {
 	createRandomAccount(t)
+}
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
