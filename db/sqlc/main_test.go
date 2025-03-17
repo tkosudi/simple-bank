f7966d48972f58e2bc0cb95a62991e7fa0d81ade
TK <tkosudi@gmail.com>
2025-03-17 15:17:39 -0300

    chore: apply simple_protocol to pgxpool config in tests

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,11 +6,13 @@ import (
 	"os"
 	"testing"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 const (
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	localDBSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	ciDBSource    = "postgresql://root:secret@postgres:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
@@ -18,9 +20,19 @@ var testDB *pgxpool.Pool
 
 func TestMain(m *testing.M) {
 	var err error
-	
-	testDB, err = pgxpool.New(context.Background(), dbSource)
+	dbSource := localDBSource
+	if os.Getenv("GITHUB_ACTIONS") == "true" {
+		dbSource = ciDBSource
+	}
 
+	config, err := pgxpool.ParseConfig(dbSource)
+	if err != nil {
+		log.Fatal("Cannot parse config: ", err)
+	}
+
+	config.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeSimpleProtocol
+
+	testDB, err = pgxpool.NewWithConfig(context.Background(), config)
 	if err != nil {
 		log.Fatal("Cannot connect to db: ", err)
 	}
