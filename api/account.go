aa00c7ca9b87481deb07f3f4d274ec28090f3968
TK <tkosudi@gmail.com>
2025-03-19 15:53:11 -0300

    feat(api): add account handlers for create and get account routes
    - Added  handler with input validation and DB integration
    - Added  handler with URI param validation and error handling (404 and 500)
    - Handlers use Gin context and call methods from store (CreateAccount, GetAccount)
    - Includes specific handling for pgx.ErrNoRows

---
+++ api/account.go
@@ -0,0 +1,64 @@
+package api
+
+import (
+	"errors"
+	"net/http"
+	db "simplebank/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" biding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=EUR USD BRL"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
