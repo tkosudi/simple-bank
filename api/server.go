4578aa670ac7bb198f405469381b8c4210e2124b
TK <tkosudi@gmail.com>
2025-03-19 15:37:15 -0300

    feat(api): create server.go and main.go to start HTTP server and DB connection

---
+++ api/server.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	db "simplebank/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for our banking service.
+// It is responsible for routing requests to the appropriate handler.
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing.
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server on a specific address.
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log"
+	"simplebank/api"
+	db "simplebank/db/sqlc"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	dbSource      = "postgresql://root:secret@127.0.0.1:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+
+	conn, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+
+}
