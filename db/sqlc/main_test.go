7de960ecbbacd4fba9e77a5924a1e19cbbf95b73
TK <tkosudi@gmail.com>
2025-03-19 15:43:54 -0300

    refactor(db): update main_test.go to load DB source from env and simplify connection

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,13 +6,11 @@ import (
 	"os"
 	"testing"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 const (
-	localDBSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	ciDBSource    = "postgresql://root:secret@127.0.0.1:5432/simple_bank?sslmode=disable"
+	DBSource = "postgresql://root:secret@127.0.0.1:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
@@ -20,24 +18,22 @@ var testDB *pgxpool.Pool
 
 func TestMain(m *testing.M) {
 	var err error
-	dbSource := localDBSource
-	if os.Getenv("GITHUB_ACTIONS") == "true" {
-		dbSource = ciDBSource
-	}
 
-	config, err := pgxpool.ParseConfig(dbSource)
+	dbSource := getDBSource()
+
+	testDB, err = pgxpool.New(context.Background(), dbSource)
 	if err != nil {
-		log.Fatal("Cannot parse config: ", err)
-	}
-
-	config.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeSimpleProtocol
-
-	testDB, err = pgxpool.NewWithConfig(context.Background(), config)
-	if err != nil {
-		log.Fatal("Cannot connect to db: ", err)
+		log.Fatal("cannot connect to db:", err)
 	}
 
 	testQueries = New(testDB)
 
 	os.Exit(m.Run())
+}
+
+func getDBSource() string {
+	if envSource := os.Getenv("DB_SOURCE"); envSource != "" {
+		return envSource
+	}
+	return DBSource
 }
